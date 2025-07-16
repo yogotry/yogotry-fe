3041edd30ac360ecafa54b8a4b5b416bfcc69381
HyunWoo Jeong <jrwedo@naver.com>
2025-07-16 16:45:57 +0900

    Setting: 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <title>Yogotry</title>
+      <link
+        rel="stylesheet"
+        as="style"
+        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+      />
+    </head>
       <body>
         {children}
       </body>
